f894daaa863e08489a3a95993f711fd235b89e9f
Hector Rios <that.hector@gmail.com>
2016-02-14 17:19:27 -0600

    Add sectioned pages to the application
    This commit adds the remaining templates to the application. What's left to do
    is add logic using AngularJS to deal with all this.

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,30 @@ var app = angular.module('uxlunch', []);
 app.config(['$interpolateProvider', function($interpolateProvider) {
     $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
 
-}]).controller('FormController', function () {
-    this.visibleSection = 1;
-});
+}]).controller('FormController', ['$scope', function ($scope) {
+    // Initialize the default values here
+    // Please adjust this to the number of sections that can be going on.
+    $scope.selectedLanguage = undefined;
+    $scope.numberOfSteps = 5;
+    $scope.currentStep = 0;
+    $scope.languageOptions = [
+        { "id": 1, "name": "English" },
+        { "id": 2, "name": "Spanish" },
+    ];
+
+    $scope.changeSection = function (newSectionNumber) {
+        $scope.currentStep = newSectionNumber;
+    };
+
+    $scope.setLanguage = function() {
+        if ($scope.selectedLanguage && $scope.selectedLanguage.id) {
+            $scope.selectedLanguage = $scope.selectedLanguage.id;
+            $scope.changeSection(1);
+        }
+    }
+
+    $scope.hasSelectedLanguage = function() {
+        // return to see if it's NOT null or NOT undefined
+        return !!($scope.selectedLanguage);
+    };
+}]);
